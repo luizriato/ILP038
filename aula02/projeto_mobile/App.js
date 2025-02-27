7c7617479bac4a80056178732fac966c872b0175
luizriato <luiz.riato@outlook.com>
2025-02-26 22:14:46 -0300

    feat: hello word
    criação e modificação de primeiro projeto mobile

--- aula02/projeto_mobile/App.js
+++ aula02/projeto_mobile/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello word!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import imagem from './assets/steve (1).png';
+import imagem1 from './assets/user.png';
+
+
+class App extends Component {
+  render() {
+    let nome = 'Luiz Riato';
+    return (
+      <View style={styles.container}>
+        <Text>Olá Mundo</Text>
+        <Text>Meu Primeiro App</Text>
+        <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>
+          Programador
+        </Text>
+        <Image source={imagem} style={styles.image} />
+        <Image source={imagem1} style={styles.image} />
+        <text>{nome}</text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    padding: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    margin: 15,
   },
 });
+
+export default App;
