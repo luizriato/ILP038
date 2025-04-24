6acafab94a95555dbd1c69da4aeb3e477069af43
luizriato <luiz.riato@outlook.com>
2025-04-23 22:09:07 -0300

    feat(aula5): arquivos de aula

--- aula05/projeto-cesta/app/app.js
+++ aula05/projeto-cesta/app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
-import Cesta from './projeto-cesta/src/telas/Cesta';
+import Cesta from '../src/telas/Cesta';
 
 export default function App(){
     return (
